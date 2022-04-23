b60a55220d3b85cdb7fb8a9d6cbac73c26fa959a
Dimitar Rachev <DesolateBG@gmail.com>
2022-04-23 22:01:03 +0300

    Initial commit

---
+++ contacts/Contact.java
@@ -0,0 +1,13 @@
+package contacts;
+
+public class Contact {
+    private String name;
+    private String surname;
+    private String number;
+
+    public Contact(String name, String surname, String number) {
+        this.name = name;
+        this.surname = surname;
+        this.number = number;
+    }
+}
---
+++ contacts/PhoneBook.java
@@ -0,0 +1,9 @@
+package contacts;
+
+public class PhoneBook {
+    Contact contact;
+
+    public PhoneBook(Contact contact) {
+        this.contact = contact;
+    }
+}
